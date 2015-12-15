73b3eea73cb7f42c5e6ec7f4cc7927f462221afd
Murredafuq <ma223ik@student.lnu.se>
2015-12-15 15:25:33 +0100

    bwi0+wia+0hbkb

--- src/model/StandardYahtzeeRules.java
+++ src/model/StandardYahtzeeRules.java
@@ -166,7 +166,8 @@ public class StandardYahtzeeRules {
         boolean res = false;
         int[] scores  = Sort(dice);
 
-        score = (1*scores [0])+(2*scores [1]) +(2*scores [1])+(3*scores [2])+(4*scores [3])+(5*scores [4])+(5*scores[6]);
+        score = (1*scores[0])+(2*scores[1])+(3*scores[2])+(4*scores[3])+
+                (5*scores[4])+(6*scores[5]);
 
         if(scores[0]== 4 || scores[1]== 4 || scores[2]== 4 || scores[3]== 4 || scores[4]== 4 || scores[5]== 4){
             res = true;
